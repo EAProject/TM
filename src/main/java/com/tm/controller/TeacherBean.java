69700ca7e84c26460a3b2eaf6888141fb8e35171
SuJoshi <SuJoshi@cs_v32-301.cs.mum.edu>
2014-11-29 11:37:02 -0600

    Teacher Edit in controller

--- src/main/java/com/tm/controller/TeacherBean.java
+++ src/main/java/com/tm/controller/TeacherBean.java
@@ -10,9 +10,13 @@ import com.tm.entities.Teacher;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.RowEditEvent;
 
 /**
  *
@@ -20,38 +24,69 @@ import javax.faces.bean.SessionScoped;
  */
 @ManagedBean(name = "teacherBean")
 @SessionScoped
-public class TeacherBean implements Serializable{
+public class TeacherBean implements Serializable {
+
     @EJB
     private TeacherFacadeLocal teacherFacadeLocal;
-    Teacher teacher=new Teacher();
-    List<Teacher> teachers=new ArrayList<Teacher>();
-    
-    public TeacherBean(){
+    Teacher teacher = new Teacher();
+    List<Teacher> teachers = new ArrayList<Teacher>();
+
+    public TeacherBean() {
         try {
-             System.out.println("IN TRY CATCH>>>>>>>>>>>>>>>>");
-             teachers=teacherFacadeLocal.findAll();
-             System.out.println("after query");
+            teachers = teacherFacadeLocal.findAll();
         } catch (NullPointerException e) {
         }
-      System.out.println("Size is >>>>>>"+teachers.size());
-        
+        System.out.println("Size is >>>>>>" + teachers.size());
+
     }
-    public String showTeacherInfo(){
+
+    public void onRowCancel(RowEditEvent event) {
+        try {
+            Teacher teacher = (Teacher) event.getObject();
+            if (teacher != null) {
+            } else {
+                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, ResourceBundle.getBundle("bundle").getString("msg.error.save"), "");
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            }
+        } catch (Exception ex) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, ResourceBundle.getBundle("bundle").getString("msg.error.save"), "");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    public void onRowEdit(RowEditEvent event) {
+        try {
+            Teacher teacher = (Teacher) event.getObject();
+            if (teacher != null) {
+                teacherFacadeLocal.edit(teacher);
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Updated Teacher"));
+            } else {
+                System.out.println("ELSE");
+                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, ResourceBundle.getBundle("bundle").getString("msg.error.save"), "");
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            }
+        } catch (Exception ex) {
+            System.out.println("CATCH");
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, ResourceBundle.getBundle("bundle").getString("msg.error.save"), "");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    public String showTeacherInfo() {
         System.out.println("Inside method");
-        teachers=teacherFacadeLocal.findAll();
-        System.out.println("SHOW TEACHER INFO "+teachers.size());
+        teachers = teacherFacadeLocal.findAll();
+        System.out.println("SHOW TEACHER INFO " + teachers.size());
         return "index";
     }
-    
-    public String addTeacher(){
+
+    public String addTeacher() {
         teacher.setIsDeleted(false);
         teacherFacadeLocal.create(teacher);
-        
-        
         clearTeacherValue();
         return "success";
     }
-    public void clearTeacherValue(){
+
+    public void clearTeacherValue() {
         teacher.setFirstName("");
         teacher.setMiddleName("");
         teacher.setLastName("");
@@ -73,6 +108,5 @@ public class TeacherBean implements Serializable{
     public void setTeachers(List<Teacher> teachers) {
         this.teachers = teachers;
     }
-    
-    
+
 }
